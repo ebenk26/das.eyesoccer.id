27d3bd10385db9924611f6b38f860f4aeb593fa7
Kharizuno <kharizuno@yahoo.com>
2018-04-19 17:55:00 +0700

    'player

--- application/views/themes/v1/member/club/header.php
+++ application/views/themes/v1/member/club/header.php
@@ -1,12 +1,12 @@
 <div class="container submenu">
 	<div class="submenus">
 		<ul>
-			<li class="<?php echo ($active=='info_klub' ? 'active' : '')?>">info klub</li>
-			<li class="<?php echo ($active=='pemain' ? 'active' : '')?>">pemain</li>
-			<li class="<?php echo ($active=='ofisial' ? 'active' : '')?>">ofisial</li>
-			<li class="<?php echo ($active=='galeri' ? 'active' : '')?>">galeri</li>
-			<li class="<?php echo ($active=='verifikasi' ? 'active' : '')?>">verifikasi</li>
-			<li class="<?php echo ($active=='daftar_liga' ? 'active' : '')?>">daftar liga</li>
+            <li class="<?php echo ($active=='klub' ? 'active' : '')?>"><a href="<?php echo base_url()?>member/klub">info klub</a></li>
+            <li class="<?php echo ($active=='pemain' ? 'active' : '')?>"><a href="<?php echo base_url()?>member/player">pemain</a></li>
+            <li class="<?php echo ($active=='ofisial' ? 'active' : '')?>"><a href="<?php echo base_url()?>member/ofisial">ofisial</a></li>
+            <li class="<?php echo ($active=='galeri' ? 'active' : '')?>"><a href="<?php echo base_url()?>member/galeri">galeri</a></li>
+            <li class="<?php echo ($active=='verifikasi' ? 'active' : '')?>"><a href="<?php echo base_url()?>member/verifikasi">verifikasi</a></li>
+            <li class="<?php echo ($active=='daftar_liga' ? 'active' : '')?>"><a href="<?php echo base_url()?>member/regliga">daftar liga</a></li>
 		</ul>
 	</div>
 </div>
---
+++ application/views/themes/v1/member/player/header.php
@@ -0,0 +1,10 @@
+<div class="container submenu">
+	<div class="submenus">
+		<ul>
+            <li class="<?php echo ($active=='profil' ? 'active' : '')?>"><a href="<?php echo base_url()?>member/player_info">profil</a></li>
+            <li class="<?php echo ($active=='karir' ? 'active' : '')?>"><a href="<?php echo base_url()?>member/player_karir">karir</a></li>
+            <li class="<?php echo ($active=='penghargaan' ? 'active' : '')?>"><a href="<?php echo base_url()?>member/player_penghargaan">penghargaan</a></li>
+            <li class="<?php echo ($active=='galeri' ? 'active' : '')?>"><a href="<?php echo base_url()?>member/player_galeri">galeri</a></li>
+		</ul>
+	</div>
+</div>
