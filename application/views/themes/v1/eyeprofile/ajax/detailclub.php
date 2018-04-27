0c47106dad3f3162398c6894525aec3126bd9f1f
unuha26 <unuha26@users.noreply.github.com>
2018-04-27 15:04:09 +0700

    ketinggalan

--- application/views/themes/v1/eyeprofile/ajax/detailclub.php
+++ application/views/themes/v1/eyeprofile/ajax/detailclub.php
@@ -16,7 +16,7 @@ div.user-data:hover{background-color:#ff990026}
 			</div>
 				<h2 class="tx-c h2-pemain-top-head"><?php echo $dt->name;?></h2>
 				<span class="button-open sbpbtn unset-btn-white" style="max-height: unset;box-shadow: 1px 2px 3px 1px #0357b5;border-radius: 30px;max-width: max-content;">Daftarkan Sebagai Pemain <?php echo $dt->name;?></span>
-							<div id="boxtab" class="container tab-sub-menu">
+							<div id="boxtab" class="container tab-sub-menu" style="position: relative;bottom: unset;margin-top: 20px;">
 								<a id="tab-info" href="javascript:void(0)" class="active" onclick="tabmenu(this.id, 'a', 'div', 'active')" active="true" style="padding: 0 8px;">Info</a>
 								<a id="tab-pemain" href="javascript:void(0)" class="tabmenu(this.id, 'a', 'div', 'active')" onclick="tabmenu(this.id, 'a', 'div', 'active')" active="true" style="border-left: 1px solid;padding: 0 8px;">Pemain</a>
 								<a id="tab-ofisial" href="javascript:void(0)" onclick="tabmenu(this.id, 'a', 'div', 'active')" style="border-left: 1px solid;border-right:  1px solid;border-color: white; padding: 0 8px;">Ofisial</a>
